ea57c35e6a649f066ac8777081845dee74e07637
mahdi-shi <4932abcd@gmail.com>
2023-03-08 13:30:11 +0330

    stating code for node side

--- public/logic/content.js
+++ public/logic/content.js
@@ -1,3 +1,13 @@
-document.querySelector("#getLocationText").addEventListener("click",()=>{
-    alert("fuck");
+document.querySelector("#getLocationText").addEventListener("click", () => {
+
+    if ("geolocation" in navigator) {
+        console.log("geolocatoin available");
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+        });
+    }
+    else{
+        console.log("geolocation not available");
+    }
+
 })
