614031e3c7f625b7446f2677f61cc8c01e78222e
mahdi-shi <4932abcd@gmail.com>
2023-03-08 18:27:42 +0330

    handling errors about taking coords about geolocation api

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const express = require('express');
 const app = express();
+const axios = require("axios").default;;
+
 app.listen(3000,()=>console.log('listen at 3000'));
 app.use(express.static('public'));
+
+app.get("/weather/:latlon", async (req, res) => {
+    console.log("fetch test");
+    const latlong = req.params.latlon.split(",");
+    console.log(latlong)
+    const Lat = latlong[0];
+    const Long = latlong[1];
+
+    const weatherResponse = await axios(`https://api.weather.gov/points/${Lat},${Long}`)
+        .catch(err => console.error("something went wrongoooooooo " + err));
+    const weatherData = await weatherResponse.data;
+
+    console.log(weatherData);
+
+    const aqResponse = await axios(`https://api.openaq.org/v2/latest?coordinates=${Lat},${Long}`)
+        .catch(err => console.error("something went wrong " + err));
+    const aqData = await aqResponse.data;
+
+    console.log(aqData);
+
+
+    const data = {
+        weather: weatherData,
+        air_quality: aqData
+    }
+
+    res.send(data)
+})
--- public/logic/content.js
+++ public/logic/content.js
@@ -1,13 +1,19 @@
-document.querySelector("#getLocationText").addEventListener("click", () => {
+document.querySelector("#getLocationText").addEventListener("click", async () => {
+
+    let response = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_SzNblVsGgWDZd6RMGXGmgxVdK6ZPw&ipAddress=8.8.8.8");
+    let Tdata = await response.json();
+    const Lat = Tdata.location.lat;
+    const Lng = Tdata.location.lng;
 
     if ("geolocation" in navigator) {
-        console.log("geolocatoin available");
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            console.log(position.coords.latitude, position.coords.longitude);
+            const Response = await axios(`/weather/${position.coords.latitude},${position.coords.longitude}`);
+            const wData = await Response.data.weather;
+            console.log(wData)
         });
-    }
-    else{
-        console.log("geolocation not available");
+    } else {
+        console.log("geoLocation not available");
     }
 
 })
