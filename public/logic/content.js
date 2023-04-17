fcc4cdf500b6b1b62244185620a26d5163fec74b
mahdi-shi <4932abcd@gmail.com>
2023-04-17 15:48:12 +0430

    adding leaflet js api and tools for create a map and it's stuff

--- public/logic/content.js
+++ public/logic/content.js
@@ -9378,3 +9378,13 @@ arrowLeftBtn.addEventListener("click", () => {
   }
   console.log(pos);
 })
+
+//adding tile for the map or raddar
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
